13b8c87b178af812530ebee6aa17c83f9b74715f
cheesechoux <gsoosuu@gmail.com>
2023-04-24 15:16:49 +0900

    feat: SignUpViewTestCase

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,53 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.core.exceptions import ValidationError
 
-# Create your tests here.
+from users.models import User
+from users.validation import phone_validate, password_validate
+import json
+
+
+class SignUpViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('signup')
+
+    def test_signup_with_valid_credentials(self):
+        data = {
+            'phone': '010-1234-5678',
+            'password': 'Jisoo1234'
+        }
+        response = self.client.post(self.url, json.dumps(
+            data), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.first().phone, data['phone'])
+
+    def test_signup_with_invalid_phone(self):
+        data = {
+            'phone': '010-1234',
+            'password': 'Jisoo1234'
+        }
+        with self.assertRaises(ValidationError) as context:
+            phone_validate(data['phone'])
+        self.assertEqual(str(context.exception), "['INVALID_PHONE_NUMBER']")
+
+    def test_signup_with_invalid_password(self):
+        data = {
+            'phone': '010-1234-5678',
+            'password': 'jisoo'
+        }
+        with self.assertRaises(ValidationError) as context:
+            password_validate(data['password'])
+        self.assertEqual(str(context.exception), "['INVALID_PASSWORD']")
+
+    def test_signup_with_existing_phone(self):
+        User.objects.create(phone='010-1234-5678', password='Jisoo1234')
+        data = {
+            'phone': '010-1234-5678',
+            'password': 'Jisoo1234'
+        }
+        response = self.client.post(self.url, json.dumps(
+            data), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(User.objects.count(), 1)
--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from users.views import SignUpView, LoginView, LogoutView
 
 urlpatterns = [
-    path('/signup', SignUpView.as_view()),
-    path('/login', LoginView.as_view()),
-    path('/logout', LogoutView.as_view()),
+    path('/signup', SignUpView.as_view(), name='signup'),
+    path('/login', LoginView.as_view(), name='login'),
+    path('/logout', LogoutView.as_view(), name='logout'),
 ]
